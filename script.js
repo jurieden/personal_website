fe6ab808a500929e7087c571d57dd75bbd8cb93c
Juri Gerber <juri@Juris-MBP.fritz.box>
2021-04-21 11:30:59 +0200

    added new pages, addes CSS, JavaScript and an image

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function myFunction(p1, p2) { return p1 * p2; }
+
+let myFirstVariable = 2
+
+let mySecondVariable = 5
+
+console.log(myFirstVariable + mySecondVariable)
+
+let numberOfClicks = 0
+
+function myFunction() {
+    numberOfClicks = numberOfClicks + 1
+    const counterContainer = document.getElementById("counter-display")
+    counterContainer.innerText = numberOfClicks
+
+    if(numberOfClicks===10){
+        counterContainer.style.color="red"
+    }
+
+}
 // comment single line
 /*
 comment
