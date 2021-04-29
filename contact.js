958ededa458eb265e579c0575579041ec54e607a
Juri Gerber <juri@Juris-MBP.fritz.box>
2021-04-29 09:44:04 +0200

    added some more JavaScript

---
+++ contact.js
@@ -0,0 +1,9 @@
+document.getElementById("contact-submit").onclick=function(){
+    console.log("Hello I was clicked")
+    const name = document.getElementById("name-input").value
+    console.log(name)
+    const phone = document.getElementById("phone-input").value
+    console.log(phone)
+    const email = document.getElementById("email-input").value
+    console.log(email)
+}
