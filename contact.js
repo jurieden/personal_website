d24b7c9089d274fc19965106029fc046a1b2ae0d
Juri Gerber <juri@Juris-MBP.fritz.box>
2021-05-26 10:53:49 +0200

    JS Form to Zapier

--- contact.js
+++ contact.js
@@ -1,7 +1,17 @@
-document.getElementById("contact-form").onsubmit=function(event){
+document.getElementById("contact-form").onsubmit = function (event) {
     event.preventDefault()
     console.log(event.target)
     console.log(event.target.name.value)
     console.log(event.target.email.value)
     console.log(event.target.phone.value)
+    fetch("https://hooks.zapier.com/hooks/catch/10212759/boin3jy/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "name": event.target.name.value,
+            "email": event.zarget.email.value
+        })
+    })
 }
