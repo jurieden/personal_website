f7571cf7dc20a6dd801ea2748e6924a5cb1c4447
Juri Gerber <juri@Juris-MacBook-Pro.fritz.box>
2021-08-17 17:13:37 +0200

    +button and adjustments on activities

--- activities.js
+++ activities.js
@@ -3,34 +3,74 @@ document.getElementById("activities").onsubmit = function (event) {
   console.log(event)
   console.log(event.target.activities.value)
   console.log(event.target.time.value)
-  fetch("https://hook.integromat.com/b7fru9rcs2secob2b57x4o5uhbdpxd19", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        mode: "no-cors",
-        body: JSON.stringify({
-            "activities": event.target.activities.value,
-            "time": event.target.time.value
-        })
-})
-fetch("https://hook.integromat.com/b7fru9rcs2secob2b57x4o5uhbdpxd19", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        mode: "no-cors",
-        body: JSON.stringify({
-            "activities": event.target.activities.value,
-            "time": event.target.time.value
-        })
-})
+
+  //let timeFilter = document.getElementById("timeDropdown").value;
+  //let categoryFilter = document.getElementById("activitiesDropdown").value;
+
+  document.getElementById("activitiesTable").innerHTML = "";
+
+  // getting our activities
+  fetch(`https://calm-spire-33184.herokuapp.com/activities?activityTime=${event.target.time.value}&activityName=${event.target.activities.value}`)
+  .then(function (response) {
+    return response.json()
+  }).then(function (data) {
+    console.log(data, "data")
+    // data
+    data.map(function (item) {
+      // Create Activities again
+      createActivitiesRow(item)
+    })
+  })
+
 }
+  /*
+  fetch("https://hook.integromat.com/b7fru9rcs2secob2b57x4o5uhbdpxd19", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    mode: "no-cors",
+    body: JSON.stringify({
+      "activities": event.target.activities.value,
+      "time": event.target.time.value
+    })
+  })
+  fetch("https://jurieden.netlify.app/activities.html?post", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    mode: "no-cors",
+    body: JSON.stringify({
+      "activities": event.target.activities.value,
+      "time": event.target.time.value
+    })
+  })*/
+
+
 
 document.getElementById("activitiesrandom").onsubmit = function (event) {
   event.preventDefault()
-  console.log("random")}
+  console.log("random")
+}
 
+
+
+
+// getting our activities when the user JUST landed on the page
+
+/*
+fetch("https://calm-spire-33184.herokuapp.com/activities")
+.then(function (response) {
+  return response.json()
+}).then(function (data) {
+  console.log(data, "data")
+  // data
+  data.map(function (item) {
+    createActivitiesRow(item)
+  })
+})
+*/
 
 function createActivitiesRow(activity) {
   const row = document.createElement("tr")
@@ -44,8 +84,12 @@ function createActivitiesRow(activity) {
   row.appendChild(secondCell)
 
   const thirdCell = document.createElement("td")
-  thirdCell.innerText = activity.activityLink
+ // thirdCell.innerText = activity.activityLink
   row.appendChild(thirdCell)
+
+  const iframe = document.createElement("iframe")
+  iframe.setAttribute("src",activity.iframeLink)
+  thirdCell.appendChild(iframe)
 
   document.getElementById("activitiesTable").appendChild(row)
 }
@@ -54,24 +98,31 @@ function createActivitiesRow(activity) {
 //createActivitiesRow("Cinema","90 min","https://www.google.com/search?q=cinema+berlin+today&oq=cinema+berlin+today")
 //createActivitiesRow("Meditation","20 min","https://www.youtube.com/watch?v=e2BKaxsX88M")
 
+/*
 const sports = {
   activityName: "Sports",
   activityTime: "10 min",
   activityLink: "https://www.youtube.com/watch?v=6VFLKdfxA24",
 }
+*/
 
 //createActivitiesRow(sports)
 
+/*
 const cinema = {
   activityName: "Cinema",
   activityTime: "90min",
   activityLink: "https://www.google.com/search?q=cinema+berlin+today&oq=cinema+berlin+today",
 }
-const listOfActivities = [sports, cinema]
+*/
+
+//const listOfActivities = [sports, cinema]
 //createActivitiesRow(cinema)
 
+/*
 listOfActivities.filter(function (item) {
   return item.activityName === "Sports"
 }).map(function (item) {
   createActivitiesRow(item)
 })
+*/
---
+++ button.js
@@ -0,0 +1,55 @@
+
+let changeme = 0
+
+function clickItFunction() {
+    changeme = changeme + 1
+    const klickMeHard = document.getElementById("klickMeHard")
+
+    if (changeme === 1) {
+        klickMeHard.innerText = "Dankeeee, vielleicht noch mal? 🥺"
+    }
+    if (changeme === 2) {
+        klickMeHard.innerText = "Ok Ok jetzt reicht es :)"
+    }
+    if (changeme === 3) {
+        klickMeHard.innerText = "Bitte hör auf.."
+    }
+    if (changeme === 4) {
+        klickMeHard.innerText = "haha das kitzelt"
+    }
+    if (changeme === 5) {
+        klickMeHard.innerText = "ahhhhh ich kann nicht mehr"
+    }
+    if (changeme === 6) {
+        klickMeHard.innerText = "OK,  ich erzähle dir von meinem Geheimnis!"
+    }
+    if (changeme === 7) {
+        klickMeHard.innerText = "Bitte hör mir genau zu"
+    }
+    if (changeme === 8) {
+        klickMeHard.innerText = "gaaag, ich sag dir guar nix"
+    }
+    if (changeme === 10) {
+        klickMeHard.innerText = "oho"
+    }
+    if (changeme === 13) {
+        klickMeHard.innerText = "Da ist aber jemand neugirig, dann sollst du auch belohnt werden"
+    }
+    if (changeme === 14) {
+        klickMeHard.innerText = "Ich bin noch ein junger Button.."
+    }
+    if (changeme === 15) {
+        klickMeHard.innerText = "Ich muss noch ein paar mal von dir geklickt werden"
+    }
+    if (changeme === 16) {
+        klickMeHard.innerText = "Bis ich dich zu einem Ziel führen kann"
+    }
+    if (changeme === 17) {
+        klickMeHard.innerText = "Yippieh! Jetzt bin ich ein richter Button"
+
+    }
+    if (changeme === 17) {
+        klickMeHard. = "Yippieh! Jetzt bin ich ein richter Button"
+
+    }
+}
